b27acd7b2627bd07d50c2585b9b24555218ee813
dankato <apearmor@gmail.com>
2017-08-10 12:30:05 -0700

    updates to package

--- server/config.js
+++ server/config.js
@@ -10,4 +10,4 @@ exports.TEST_DATABASE_URL = (
   process.env.TEST_DATABASE_URL ||
  'mongodb://localhost/test');
  console.log(exports.DATABASE_URL, 'database url');
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 3001;
